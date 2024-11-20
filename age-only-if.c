62b011a65bf3762c14ff619be4d00657199f138a
lal-jeet <laljeetofficialgithub@gmail.com>
2024-11-20 16:43:45 +0530

    Add files via upload
    condition program if

---
+++ age-only-if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int age;
+   char name[20];
+   printf("Enetr your name :");
+   scanf("%s",&name);
+   printf("Enetr your age :");
+   scanf("%d",&age);
+   
+   if(age>= 18){ printf("hi %s you are eligble for vote",name);}
+   if(age<18)
+   { printf("sorry %s you are not eligble for vote",name);}
+
+    return 0;
+}
